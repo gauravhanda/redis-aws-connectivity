e06f5271d0c59de1090490c4a37ad37ff0fc6dd5
Gaurav Handa <gauravhanda21@yahoo.com>
2022-04-01 15:42:55 -0500

    First commit
    First commit

---
+++ src/main/java/com/example/redis/connector/RedisApplication.java
@@ -0,0 +1,49 @@
+package com.example.redis.connector;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+
+import java.util.Arrays;
+import java.util.Set;
+
+@SpringBootApplication
+public class RedisApplication implements CommandLineRunner {
+    private final static Logger logger = LoggerFactory.getLogger(RedisApplication.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting Application");
+        SpringApplication.run(RedisApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+            String node1 = System.getenv("REDIS_NODE1");
+            String node2 = System.getenv("REDIS_NODE2");
+            String node3 = System.getenv("REDIS_NODE3");
+            int port = Integer.parseInt(System.getenv("REDIS_PORT"));
+            logger.info("Adding nodes {} , {}, {} -> Port = {}", node1, node2, node3, port);
+            Set<HostAndPort> hostAndPorts = Set.copyOf(Arrays.asList(new HostAndPort(node1, port),
+                    new HostAndPort(node2, port),
+                    new HostAndPort(node3, port)));
+
+            // int connectionTimeout, int soTimeout, int maxAttempts, String password, String clientName, GenericObjectPoolConfig<Connection> poolConfig, boolean ssl)
+            int timeOuts = 10000;
+            JedisCluster cluster = new JedisCluster(hostAndPorts, timeOuts, timeOuts, 2, "",
+                    "EC2-client", null, true);
+
+            String str = cluster.set("BESTIES", "ROMEO/JULIET");
+            logger.info(str);
+
+            logger.info("Invoking Business Application");
+        } catch (Exception ex) {
+            logger.info("Failed to get connection", ex);
+        }
+
+    }
+}
